18dc773917cb39ea8f660bda469d069e60822fcc
Артем Черных <acac1212@yandex.ru>
2022-10-03 17:58:22 +1000

    start code index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-const genDiff = (file1, file2) => {
+import fs from 'fs';
+import _ from 'lodash';
+
+const genDiff = (filepath1, fileopath2) => {
+  const file1 = JSON.parse(fs.readFileSync(filepath1, 'utf-8'));
+  const file2 = JSON.parse(fs.readFileSync(filepath2, 'utf-8'));
+  const keys = _.union(_.keys(file1), _.keys(file2));
 };
 
 export default genDiff;
