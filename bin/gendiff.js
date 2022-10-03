6b3b02c9accad3aeee6636c2475d280fdc72dd0b
Артем Черных <acac1212@yandex.ru>
2022-10-03 12:26:32 +1000

    fix gendiff.js. Fix parse

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .parse(process.argv)
+  
 
-program.parse();
