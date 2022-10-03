007d20562c7b01ad537be9f2012a1d4837ff5ed8
Артем Черных <acac1212@yandex.ru>
2022-10-03 12:43:46 +1000

    fix gendiff.js. fix arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,8 +7,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .parse(process.argv)
   
 
