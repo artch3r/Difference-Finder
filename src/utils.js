7e934defe01d1deb21bdde9c84d7aaab3849a56a
Артем Черных <acac1212@yandex.ru>
2022-10-05 17:24:04 +1000

    fix getFixturePath

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__tests__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename));
 
