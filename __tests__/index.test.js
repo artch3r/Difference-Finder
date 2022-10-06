3b2d023105da7adaea53087acfca8be7dc9add20
Артем Черных <acac1212@yandex.ru>
2022-10-06 10:23:20 +1000

    add index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,28 @@
-import { beforeAll } from '@jest/globals';
-import { getFixturePath, readFile } from '../src/utils.js';
+import { beforeAll, expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
 
-let file1;
-let file2;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+let filepath1;
+let filepath2;
 
 beforeAll(() => {
-  file1 = readFile((getFixturePath(file1.json)));
-  file2 = readFile((getFixturePath(file2.json)));
+  filepath1 = getFixturePath('file1.json');
+  filepath2 = getFixturePath('file2.json');
+});
+
+test('genDiff', () => {
+  expect(genDiff(filepath1, filepath2)).toEqual(`{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`);
 });
