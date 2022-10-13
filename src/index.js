2b7cee132b6b2931201b3c990c02b05da17a754c
Артем Черных <acac1212@yandex.ru>
2022-10-13 17:15:24 +1000

    delete comments of another internal view

--- src/index.js
+++ src/index.js
@@ -35,59 +35,6 @@ const checkIsObject = (data) => _.isObject(data) && !Array.isArray(data);
 const getDiff = (parsedFile1, parsedFile2) => {
   const keys = _.union(_.keys(parsedFile1), _.keys(parsedFile2));
   const sortedKeys = _.sortBy(keys);
-  // Формирование дифа-массива с элементами-объектами
-  // вида { key: 'follow', status: '+', value: 'false' },
-  // value может содержать массив с детьми-объектами
-  //
-  // const differences = sortedKeys.reduce((acc, key) => {
-  //   const value1 = parsedFile1[key];
-  //   const value2 = parsedFile2[key];
-
-  //   if (!_.has(parsedFile2, key)) {
-  //     return [...acc, { key, status: '-', value: value1 }];
-  //   }
-
-  //   if (!_.has(parsedFile1, key)) {
-  //     return [...acc, { key, status: '+', value: value2 }];
-  //   }
-
-  //   if (checkIsObject(value1) && checkIsObject(value2)) {
-  //     return [...acc, { key, status: ' ', value: getDiff(value1, value2) }];
-  //   }
-
-  //   return parsedFile1[key] === parsedFile2[key]
-  //     ? [...acc, { key, status: ' ', value: value1 }]
-  //     : [...acc, { key, status: '-', value: value1 }, { key, status: '+', value: value2 }];
-  // }, []);
-  //
-  //
-  // Формирование дифа-объекта типа
-  // group1: {
-  //   status: 'nested',
-  //   value: { baz: [Object], foo: [Object], nest: [Object] }
-  // },
-  //
-  // const differences = sortedKeys.reduce((acc, key) => {
-  //   const value1 = parsedFile1[key];
-  //   const value2 = parsedFile2[key];
-
-  //   if (!_.has(parsedFile2, key)) {
-  //     return { ...acc, [key]: { status: 'deleted', value: value1 } };
-  //   }
-
-  //   if (!_.has(parsedFile1, key)) {
-  //     return { ...acc, [key]: { status: 'added', value: value2 } };
-  //   }
-
-  //   if (checkIsObject(value1) && checkIsObject(value2)) {
-  //     return { ...acc, [key]: { status: 'nested', value: getDiff(value1, value2) } };
-  //   }
-
-  //   return parsedFile1[key] === parsedFile2[key]
-  //     ? { ...acc, [key]: { status: 'unchanged', value: value1 } }
-  //     : { ...acc, [key]: { status: 'changed', oldValue: value1, newValue: value2 } };
-  // }, {});
-
   const differences = sortedKeys.reduce((acc, key) => {
     const value1 = parsedFile1[key];
     const value2 = parsedFile2[key];
--- src/stylish.js
+++ src/stylish.js
@@ -22,63 +22,6 @@ const stringify = (value, depth) => {
 };
 
 const stylish = (data) => {
-  // Отображение дифа-массива с элементами-объектами
-  // вида { key: 'follow', status: '+', value: 'false' },
-  // value может содержать массив с детьми-объектами
-  //
-  // const iter = (currentElement, depth) => {
-  //   if (!Array.isArray(currentElement)) {
-  //     return stringify(currentElement, depth + 1);
-  //   }
-
-  //   const indentSize = depth * 2;
-  //   const currentIndent = ' '.repeat(indentSize);
-  //   const bracketIndent = ' '.repeat(indentSize - 2);
-
-  //   const lines = currentElement.map(({ key, status, value }) =>
-  // `${currentIndent}${status} ${key}: ${iter(value, depth + 2)}`);
-
-  //   return ['{', ...lines, `${bracketIndent}}`].join('\n');
-  // };
-  //
-  //
-  // Формирование дифа-объекта типа
-  // group1: {
-  //   status: 'nested',
-  //   value: { baz: [Object], foo: [Object], nest: [Object] }
-  // },
-  //
-  // const iter = (currentElement, depth) => {
-  //   const indentSize = depth * 2;
-  //   const currentIndent = ' '.repeat(indentSize);
-  //   const bracketIndent = ' '.repeat(indentSize - 2);
-
-  //   const keys = _.keys(currentElement);
-  //   const lines = keys.flatMap((key) => {
-  //     const {
-  //       status, value, oldValue, newValue,
-  //     } = currentElement[key];
-
-  //     switch (status) {
-  //       case 'deleted':
-  //         return `${currentIndent}- ${key}: ${stringify(value, depth + 3)}`;
-  //       case 'added':
-  //         return `${currentIndent}+ ${key}: ${stringify(value, depth + 3)}`;
-  //       case 'unchanged':
-  //         return `${currentIndent}  ${key}: ${stringify(value, depth + 3)}`;
-  //       case 'changed':
-  //         return [`${currentIndent}- ${key}: ${stringify(oldValue, depth + 3)}`,
-  //  `${currentIndent}+ ${key}: ${stringify(newValue, depth + 3)}`];
-  //       case 'nested':
-  //         return `${currentIndent}  ${key}: ${iter(value, depth + 2)}`;
-  //       default:
-  //         throw new Error('Unknown status');
-  //     }
-  //   });
-
-  //   return ['{', ...lines, `${bracketIndent}}`].join('\n');
-  // };
-
   const iter = (currentValue, depth) => {
     const indentSize = getIndentSize(depth);
     const currentIndent = ' '.repeat(indentSize);
