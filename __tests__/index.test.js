079e1f9eab540e077734bfcc4d40c45c0a9696fb
Артем Черных <acac1212@yandex.ru>
2022-10-17 15:40:28 +1000

    fix linter in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,14 @@
-import { describe, expect, test } from '@jest/globals';
+import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
-import fs, { readFileSync } from 'fs';
+import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const jsonFilepath1 = getFixturePath('file1.json');
 const jsonFilepath2 = getFixturePath('file2.json');
