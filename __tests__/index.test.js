1ea903d8efd02d9dc435f2ac351f1a6924dfecdd
Артем Черных <acac1212@yandex.ru>
2022-10-17 15:37:46 +1000

    fix tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,129 +1,32 @@
-import { expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import fs, { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-let filepath1;
-let filepath2;
+const jsonFilepath1 = getFixturePath('file1.json');
+const jsonFilepath2 = getFixturePath('file2.json');
+const ymlFilepath1 = getFixturePath('file1.yml');
+const ymlFilepath2 = getFixturePath('file2.yml');
+const stylishDiff = readFile('stylish_diff.yml');
+const plainDiff = readFile('plain_diff.yml');
+const jsonDiff = readFile('json_diff.json');
 
-test('genDiff json', () => {
-  filepath1 = getFixturePath('file1.json');
-  filepath2 = getFixturePath('file2.json');
-
-  expect(genDiff(filepath1, filepath2)).toEqual(`{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`);
+test('Default stylish output format', () => {
+  expect(genDiff(jsonFilepath1, jsonFilepath2)).toEqual(stylishDiff);
 });
 
-test('genDiff yaml', () => {
-  filepath1 = getFixturePath('file1.yml');
-  filepath2 = getFixturePath('file2.yml');
-
-  expect(genDiff(filepath1, filepath2)).toEqual(`{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`);
-});
-
-test('gendiff plain', () => {
-  filepath1 = getFixturePath('file1.yml');
-  filepath2 = getFixturePath('file2.yml');
-
-  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(`Property 'common.follow' was added with value: false
-Property 'common.setting2' was removed
-Property 'common.setting3' was updated. From true to null
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group1.nest' was updated. From [complex value] to 'str'
-Property 'group2' was removed
-Property 'group3' was added with value: [complex value]`);
+test.each([
+  [jsonFilepath1, jsonFilepath2, stylishDiff, 'stylish'],
+  [ymlFilepath1, ymlFilepath2, stylishDiff, 'stylish'],
+  [jsonFilepath1, ymlFilepath2, plainDiff, 'plain'],
+  [ymlFilepath1, jsonFilepath2, jsonDiff, 'json'],
+])('gendiff', (filepath1, filepath2, expected, outputFormat) => {
+  expect(genDiff(filepath1, filepath2, outputFormat)).toEqual(expected);
 });
