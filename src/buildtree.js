63a93fea0b7c2b8e206d9270ecadfacafad1076c
Артем Черных <acac1212@yandex.ru>
2022-10-26 12:23:21 +1000

    fix body of sortedKeys reduce

--- src/buildtree.js
+++ src/buildtree.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
+
   return sortedKeys.reduce((acc, key) => {
     const value1 = data1[key];
     const value2 = data2[key];
@@ -19,14 +20,16 @@ const buildTree = (data1, data2) => {
       return { ...acc, children: [...acc.children, { key, type: 'nested', children: buildTree(value1, value2).children }] };
     }
 
-    return data1[key] === data2[key]
-      ? { ...acc, children: [...acc.children, { key, type: 'unchanged', value: value1 }] }
-      : {
+    if (!_.isEqual(data1[key], data2[key])) {
+      return {
         ...acc,
         children: [...acc.children, {
           key, type: 'changed', value1, value2,
         }],
       };
+    }
+
+    return { ...acc, children: [...acc.children, { key, type: 'unchanged', value: value1 }] };
   }, { type: 'root', key: '', children: [] });
 };
 
