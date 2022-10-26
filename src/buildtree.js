4b3bf52982207e3948e53082c2764d3432b5931e
Артем Черных <acac1212@yandex.ru>
2022-10-26 12:12:32 +1000

    remove checkIsObject

--- src/buildtree.js
+++ src/buildtree.js
@@ -1,27 +1,25 @@
 import _ from 'lodash';
 
-const checkIsObject = (data) => _.isObject(data) && !Array.isArray(data);
-
-const buildTree = (parsedFile1, parsedFile2) => {
-  const keys = _.union(_.keys(parsedFile1), _.keys(parsedFile2));
+const buildTree = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
-  const differences = sortedKeys.reduce((acc, key) => {
-    const value1 = parsedFile1[key];
-    const value2 = parsedFile2[key];
+  return sortedKeys.reduce((acc, key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
 
-    if (!_.has(parsedFile2, key)) {
+    if (!_.has(data2, key)) {
       return { ...acc, children: [...acc.children, { key, type: 'deleted', value: value1 }] };
     }
 
-    if (!_.has(parsedFile1, key)) {
+    if (!_.has(data1, key)) {
       return { ...acc, children: [...acc.children, { key, type: 'added', value: value2 }] };
     }
 
-    if (checkIsObject(value1) && checkIsObject(value2)) {
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { ...acc, children: [...acc.children, { key, type: 'nested', children: buildTree(value1, value2).children }] };
     }
 
-    return parsedFile1[key] === parsedFile2[key]
+    return data1[key] === data2[key]
       ? { ...acc, children: [...acc.children, { key, type: 'unchanged', value: value1 }] }
       : {
         ...acc,
@@ -30,8 +28,6 @@ const buildTree = (parsedFile1, parsedFile2) => {
         }],
       };
   }, { type: 'root', key: '', children: [] });
-
-  return differences;
 };
 
 export default buildTree;
