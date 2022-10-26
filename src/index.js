e927e6685701663b7a913154a31405655e3cec49
Артем Черных <acac1212@yandex.ru>
2022-10-26 11:50:06 +1000

    move absolutePath to getData

--- src/index.js
+++ src/index.js
@@ -11,10 +11,8 @@ const extractFormat = (filepath) => path.parse(filepath).ext.slice(1);
 const getData = (filepath) => parseData(fs.readFileSync(filepath), extractFormat(filepath));
 
 const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-  const absolutePath1 = buildAbsolutePath(filepath1);
-  const absolutePath2 = buildAbsolutePath(filepath2);
-  const data1 = getData(absolutePath1);
-  const data2 = getData(absolutePath2);
+  const data1 = getData(buildAbsolutePath(filepath1));
+  const data2 = getData(buildAbsolutePath(filepath2));
   const diffTree = buildTree(data1, data2);
 
   return format(diffTree, outputFormat);
