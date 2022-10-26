a5bca5ae1e1dd2856be6618a6d4c4dadfd59d390
Артем Черных <acac1212@yandex.ru>
2022-10-26 13:04:41 +1000

    fix buildtree.js

--- src/buildtree.js
+++ src/buildtree.js
@@ -1,36 +1,35 @@
 import _ from 'lodash';
 
-const buildTree = (data1, data2) => {
+const buildDiff = (data1, data2) => {
   const keys = _.union(_.keys(data1), _.keys(data2));
   const sortedKeys = _.sortBy(keys);
 
-  return sortedKeys.reduce((acc, key) => {
+  return sortedKeys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
     if (!_.has(data2, key)) {
-      return { ...acc, children: [...acc.children, { key, type: 'deleted', value: value1 }] };
+      return { key, type: 'deleted', value: value1 };
     }
 
     if (!_.has(data1, key)) {
-      return { ...acc, children: [...acc.children, { key, type: 'added', value: value2 }] };
+      return { key, type: 'added', value: value2 };
     }
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { ...acc, children: [...acc.children, { key, type: 'nested', children: buildTree(value1, value2).children }] };
+      return { key, type: 'nested', children: buildDiff(value1, value2) };
     }
 
     if (!_.isEqual(data1[key], data2[key])) {
       return {
-        ...acc,
-        children: [...acc.children, {
-          key, type: 'changed', value1, value2,
-        }],
+        key, type: 'changed', value1, value2,
       };
     }
 
-    return { ...acc, children: [...acc.children, { key, type: 'unchanged', value: value1 }] };
-  }, { type: 'root', key: '', children: [] });
+    return { key, type: 'unchanged', value: value1 };
+  });
 };
+
+const buildTree = (data1, data2) => ({ type: 'root', key: '', children: buildDiff(data1, data2) });
 
 export default buildTree;
