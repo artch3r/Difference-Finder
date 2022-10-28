ddf827a6c7dc8574709cf73325391b14f59622ad
Артем Черных <acac1212@yandex.ru>
2022-10-28 14:43:57 +1000

    move value, value1, value2 in buildtree.js

--- src/buildtree.js
+++ src/buildtree.js
@@ -5,16 +5,18 @@ const buildDiff = (data1, data2) => {
   const sortedKeys = _.sortBy(keys);
 
   return sortedKeys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-
     if (!_.has(data2, key)) {
-      return { key, type: 'deleted', value: value1 };
+      const value = data1[key];
+      return { key, type: 'deleted', value };
     }
 
     if (!_.has(data1, key)) {
-      return { key, type: 'added', value: value2 };
+      const value = data2[key];
+      return { key, type: 'added', value };
     }
+
+    const value1 = data1[key];
+    const value2 = data2[key];
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { key, type: 'nested', children: buildDiff(value1, value2) };
