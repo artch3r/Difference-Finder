6fd44b7666447edbaf4b46b41818d193e600a781
Артем Черных <acac1212@yandex.ru>
2022-10-29 14:24:17 +1000

    continue refactoring stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,79 +1,48 @@
 import _ from 'lodash';
 
-const indent = (depth, spacesCount = 4) => depth * spacesCount - 2;
+const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
 const stringify = (value, depth) => {
   if (!_.isObject(value)) {
     return value;
   }
 
-  const indentSize = indent(depth);
-  const currentIndent = ' '.repeat(indentSize);
-  const bracketIndent = ' '.repeat(indentSize - 2);
   const lines = Object
     .entries(value)
-    .map(([key, val]) => `${currentIndent}  ${key}: ${stringify(val, depth + 1)}`);
+    .map(([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
 
   return [
     '{',
     ...lines,
-    `${bracketIndent}}`,
+    `  ${indent(depth)}}`,
   ].join('\n');
 };
 
 const stylish = (data) => {
   const iter = (currentValue, depth) => {
-    const indentSize = indent(depth);
-    const currentIndent = ' '.repeat(indentSize);
-    const bracketIndent = ' '.repeat(indentSize - 2);
-
     const {
       key, value, value1, value2, type, children,
     } = currentValue;
 
     switch (type) {
       case 'deleted':
-        return `${currentIndent}- ${key}: ${stringify(value, depth + 1)}`;
+        return `${indent(depth)}- ${key}: ${stringify(value, depth)}`;
       case 'added':
-        return `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`;
+        return `${indent(depth)}+ ${key}: ${stringify(value, depth)}`;
       case 'unchanged':
-        return `${currentIndent}  ${key}: ${stringify(value, depth + 1)}`;
+        return `${indent(depth)}  ${key}: ${stringify(value, depth)}`;
       case 'changed':
-        return [`${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`, `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`];
+        return [`${indent(depth)}- ${key}: ${stringify(value1, depth)}`, `${indent(depth)}+ ${key}: ${stringify(value2, depth)}`];
       case 'nested':
-        return `${currentIndent}  ${key}: ${['{', ...children.flatMap((child) => iter(child, depth + 1)), `${currentIndent}  }`].join('\n')}`;
+        return `${indent(depth)}  ${key}: ${['{', ...children.flatMap((child) => iter(child, depth + 1)), `${indent(depth)}  }`].join('\n')}`;
       case 'root':
-        return ['{', ...children.flatMap((child) => iter(child, depth)), `${bracketIndent}}`].join('\n');
+        return ['{', ...children.flatMap((child) => iter(child, depth + 1)), '}'].join('\n');
       default:
         throw new Error('Unknown type');
     }
-
-    // const lines = currentValue.children.flatMap((child) => {
-    //   const {
-    //     key, value, value1, value2, type,
-    //   } = child;
-
-    //   switch (type) {
-    //     case 'deleted':
-    //       return `${currentIndent}- ${key}: ${stringify(value, depth + 1)}`;
-    //     case 'added':
-    //       return `${currentIndent}+ ${key}: ${stringify(value, depth + 1)}`;
-    //     case 'unchanged':
-    //       return `${currentIndent}  ${key}: ${stringify(value, depth + 1)}`;
-    //     case 'changed':
-    //       return [`${currentIndent}- ${key}: ${stringify(value1, depth + 1)}`,
-    //  `${currentIndent}+ ${key}: ${stringify(value2, depth + 1)}`];
-    //     case 'nested':
-    //       return `${currentIndent}  ${key}: ${iter(child, depth + 1)}`;
-    //     default:
-    //       throw new Error('Unknown type');
-    //   }
-    // });
-
-    // return ['{', ...lines, `${bracketIndent}}`].join('\n');
   };
 
-  return iter(data, 1);
+  return iter(data, 0);
 };
 
 export default stylish;
