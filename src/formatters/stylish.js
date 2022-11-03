cb6e577e73a304679278885c0a4b4d2defdc5e56
Артем Черных <acac1212@yandex.ru>
2022-11-03 12:10:57 +1000

    fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,39 +2,44 @@ import _ from 'lodash';
 
 const indent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
-const stringify = (value, depth) => {
+const stringify = (value, depth, formate) => {
   if (!_.isObject(value)) {
     return value;
   }
 
   const lines = Object
     .entries(value)
-    .map(([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
+    .map(([key, val]) => {
+      const preparedValue = { key, value: val, type: 'unchanged' };
+      return formate(preparedValue, depth + 1);
+    });
 
-  return [
-    '{',
-    ...lines,
-    `  ${indent(depth)}}`,
-  ].join('\n');
+  return `{
+${[...lines].join('\n')}
+  ${indent(depth)}}`;
 };
 
 const iter = (currentValue, depth) => {
   switch (currentValue.type) {
     case 'deleted':
-      return `${indent(depth)}- ${currentValue.key}: ${stringify(currentValue.value, depth)}`;
+      return `${indent(depth)}- ${currentValue.key}: ${stringify(currentValue.value, depth, iter)}`;
     case 'added':
-      return `${indent(depth)}+ ${currentValue.key}: ${stringify(currentValue.value, depth)}`;
+      return `${indent(depth)}+ ${currentValue.key}: ${stringify(currentValue.value, depth, iter)}`;
     case 'unchanged':
-      return `${indent(depth)}  ${currentValue.key}: ${stringify(currentValue.value, depth)}`;
+      return `${indent(depth)}  ${currentValue.key}: ${stringify(currentValue.value, depth, iter)}`;
     case 'changed': {
-      const output1 = `${indent(depth)}- ${currentValue.key}: ${stringify(currentValue.value1, depth)}`;
-      const output2 = `${indent(depth)}+ ${currentValue.key}: ${stringify(currentValue.value2, depth)}`;
+      const output1 = `${indent(depth)}- ${currentValue.key}: ${stringify(currentValue.value1, depth, iter)}`;
+      const output2 = `${indent(depth)}+ ${currentValue.key}: ${stringify(currentValue.value2, depth, iter)}`;
       return [output1, output2];
     }
     case 'nested':
-      return `${indent(depth)}  ${currentValue.key}: ${['{', ...currentValue.children.flatMap((child) => iter(child, depth + 1)), `${indent(depth)}  }`].join('\n')}`;
+      return `${indent(depth)}  ${currentValue.key}: {
+${[...currentValue.children.flatMap((child) => iter(child, depth + 1))].join('\n')}
+  ${indent(depth)}}`;
     case 'root':
-      return ['{', ...currentValue.children.flatMap((child) => iter(child, depth + 1)), '}'].join('\n');
+      return `{
+${[...currentValue.children].flatMap((child) => iter(child, depth + 1)).join('\n')}
+}`;
     default:
       throw new Error(`${currentValue.type} is unknown type`);
   }
