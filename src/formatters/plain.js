b93864c3479df8e35355fba1d6db0c429460e775
Артем Черных <acac1212@yandex.ru>
2022-11-03 12:14:00 +1000

    remove destructuring in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,24 +14,21 @@ const getValueView = (value) => {
 
 const plain = (data) => {
   const iter = (currentValue, parents) => {
-    const {
-      key, value, value1, value2, type, children,
-    } = currentValue;
     const previousPath = parents === '' ? '' : `${parents}.`;
 
-    switch (type) {
+    switch (currentValue.type) {
       case 'deleted':
         return `Property '${previousPath}${currentValue.key}' was removed`;
       case 'added':
-        return `Property '${previousPath}${currentValue.key}' was added with value: ${getValueView(value)}`;
+        return `Property '${previousPath}${currentValue.key}' was added with value: ${getValueView(currentValue.value)}`;
       case 'changed':
-        return `Property '${previousPath}${currentValue.key}' was updated. From ${getValueView(value1)} to ${getValueView(value2)}`;
+        return `Property '${previousPath}${currentValue.key}' was updated. From ${getValueView(currentValue.value1)} to ${getValueView(currentValue.value2)}`;
       case 'unchanged':
         return [];
       case 'nested':
-        return children.flatMap((child) => iter(child, `${previousPath}${key}`)).join('\n');
+        return currentValue.children.flatMap((child) => iter(child, `${previousPath}${currentValue.key}`)).join('\n');
       case 'root':
-        return children.flatMap((child) => iter(child, '')).join('\n');
+        return currentValue.children.flatMap((child) => iter(child, '')).join('\n');
       default:
         throw new Error('Unknown type');
     }
