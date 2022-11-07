581406f51e15d5581649b356330e10075f016772
Артем Черных <acac1212@yandex.ru>
2022-11-07 11:07:47 +1000

    fix buildPath in plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,29 +12,33 @@ const stringify = (value) => {
   return `${value}`;
 };
 
-const buildPreviousPath = (parents) => (parents === '' ? '' : `${parents}.`);
+const buildPath = (parents, current) => {
+  if (parents === '') {
+    return current;
+  }
 
-const plain = (data) => {
-  const iter = (currentValue, parents) => {
-    switch (currentValue.type) {
-      case 'deleted':
-        return `Property '${buildPreviousPath(parents)}${currentValue.key}' was removed`;
-      case 'added':
-        return `Property '${buildPreviousPath(parents)}${currentValue.key}' was added with value: ${stringify(currentValue.value)}`;
-      case 'changed':
-        return `Property '${buildPreviousPath(parents)}${currentValue.key}' was updated. From ${stringify(currentValue.value1)} to ${stringify(currentValue.value2)}`;
-      case 'unchanged':
-        return [];
-      case 'nested':
-        return currentValue.children.flatMap((child) => iter(child, `${buildPreviousPath(parents)}${currentValue.key}`)).join('\n');
-      case 'root':
-        return currentValue.children.flatMap((child) => iter(child, '')).join('\n');
-      default:
-        throw new Error('Unknown type');
-    }
-  };
-
-  return iter(data, '');
+  return [parents, current].join('.');
 };
+
+const iter = (currentValue, parents) => {
+  switch (currentValue.type) {
+    case 'deleted':
+      return `Property '${buildPath(parents, currentValue.key)}' was removed`;
+    case 'added':
+      return `Property '${buildPath(parents, currentValue.key)}' was added with value: ${stringify(currentValue.value)}`;
+    case 'changed':
+      return `Property '${buildPath(parents, currentValue.key)}' was updated. From ${stringify(currentValue.value1)} to ${stringify(currentValue.value2)}`;
+    case 'unchanged':
+      return [];
+    case 'nested':
+      return currentValue.children.flatMap((child) => iter(child, buildPath(parents, currentValue.key))).join('\n');
+    case 'root':
+      return currentValue.children.flatMap((child) => iter(child, '')).join('\n');
+    default:
+      throw new Error('Unknown type');
+  }
+};
+
+const plain = (data) => iter(data, '');
 
 export default plain;
